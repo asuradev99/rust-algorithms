3e55256d0760f3f5f7181853326a3d862ae7f802
Aditya Suresh <ethanaditya@gmail.com>
2021-06-05 10:24:25 -0600

    refactoring

--- src/fft.rs
+++ src/fft.rs
@@ -9,56 +9,45 @@ type C64 = Complex<f64>;
 
 #[derive(Debug, Clone)]
 pub struct RootOfUnity {
-    pub n: u64,
-    k: u64
+    pub n: usize,
+    k: usize,
 }
 
 impl RootOfUnity {
-    pub fn new(n: u64) -> Self {
+    pub fn new( n: usize) -> Self {
         RootOfUnity {
             n: n,
-            k: 1
+            k: 1,
         }
     }
     fn eval(&self) -> C64{
         // PRECOMPUTE e^(2pi) in future refactoring
         Complex::exp(Complex::new(0.0, 2 as f64 * self.k as f64 * PI / self.n as f64))
     }
-    fn pow(&self, j: u64) -> RootOfUnity {
+    fn pow(&self, j: usize) -> RootOfUnity {
         RootOfUnity {
             n: self.n,
-            k: self.k * j
+            k: self.k * j,
         }
     }
 }
 
-pub fn fft (p: Vec<u8>,  z: &RootOfUnity) -> Vec<C64>{
-
+pub fn fft (p: &Vec<u8>,  z: &RootOfUnity) -> Vec<C64>{
     if z.n == z.k {
-        return vec![Complex::new((p.iter().sum::<u8>()) as f64, 0.0 )];
+        return vec![Complex::new((p.iter().sum::<u8>()) as f64, 0.0)];
     } 
-
-    let (mut e, mut o): (Vec<u8>, Vec<u8>) = (Vec::new(), Vec::new());
-
+    let mut a: [Vec<u8>; 2] = [Vec::new(), Vec::new()];
     for i in 0..(p.len()) {
-        if i % 2 == 0 {
-            e.push(p[i]);
-        }
-        else {
-            o.push(p[i]);
-        }
+        a[i % 2].push(p[i]);
     }
-
-    let E = fft(e, &z.pow(2));
-    let O = fft(o, &z.pow(2));
-
+    let E = fft(&a[0], &z.pow(2));
+    let O = fft(&a[1], &z.pow(2));
     let C = z.n / z.k; 
-    let mut P: Vec<C64> = vec![Complex::new(0.0, 0.0); C as usize];
+    let mut P: Vec<C64> = vec![Complex::new(0.0, 0.0); C];
     for i in 0..(C / 2) {
-        P[i as usize] = E[i as usize] + z.pow(i).eval() * O[i as usize]; 
-        P[(i + (C / 2)) as usize] = E[i as usize] - z.pow(i).eval() * O[i as usize]; 
+        P[i] = E[i] + z.pow(i).eval() * O[i]; 
+        P[(i + (C / 2))] = E[i] - z.pow(i).eval() * O[i]; 
     }
-
     return P
 }
 
