c8b6c3d7223160a5218c96f8aed2dd2d27dd9c73
Aditya Suresh <ethanaditya@gmail.com>
2021-06-05 08:56:00 -0600

    minor refactoring

--- src/fft.rs
+++ src/fft.rs
@@ -4,6 +4,9 @@ use std::f64::consts::PI;
 
 use num::complex::Complex;
 
+
+type C64 = Complex<f64>;
+
 #[derive(Debug, Clone)]
 pub struct RootOfUnity {
     pub n: u64,
@@ -17,7 +20,7 @@ impl RootOfUnity {
             k: 1
         }
     }
-    fn eval(&self) -> Complex<f64>{
+    fn eval(&self) -> C64{
         // PRECOMPUTE e^(2pi) in future refactoring
         Complex::exp(Complex::new(0.0, 2 as f64 * self.k as f64 * PI / self.n as f64))
     }
@@ -29,15 +32,13 @@ impl RootOfUnity {
     }
 }
 
-pub fn fft (p: Vec<u8>,  z: &RootOfUnity) -> Vec<Complex<f64>>{
+pub fn fft (p: Vec<u8>,  z: &RootOfUnity) -> Vec<C64>{
 
     if z.n == z.k {
-        let v = p.iter().sum::<u8>();
-        return vec![Complex::new(v as f64, 0.0 )];
+        return vec![Complex::new((p.iter().sum::<u8>()) as f64, 0.0 )];
     } 
 
-    let mut e: Vec<u8> = Vec::new();
-    let mut o: Vec<u8> = Vec::new();
+    let (mut e, mut o): (Vec<u8>, Vec<u8>) = (Vec::new(), Vec::new());
 
     for i in 0..(p.len()) {
         if i % 2 == 0 {
@@ -52,16 +53,10 @@ pub fn fft (p: Vec<u8>,  z: &RootOfUnity) -> Vec<Complex<f64>>{
     let O = fft(o, &z.pow(2));
 
     let C = z.n / z.k; 
-
-    println!("{}", C);
-
-    let mut P: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); C as usize];
-
-    println!("{}", P.len() / 2);
-
+    let mut P: Vec<C64> = vec![Complex::new(0.0, 0.0); C as usize];
     for i in 0..(C / 2) {
-        P[i as usize] = E[i as usize] + z.pow(i as u64).eval() * O[i as usize]; 
-        P[(i as u64 + (C / 2)) as usize] = E[i as usize] - z.pow(i as u64).eval() * O[i as usize]; 
+        P[i as usize] = E[i as usize] + z.pow(i).eval() * O[i as usize]; 
+        P[(i + (C / 2)) as usize] = E[i as usize] - z.pow(i).eval() * O[i as usize]; 
     }
 
     return P
