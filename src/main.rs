60667934da77323873aaeffe8f577146ea627475
Aditya Suresh <ethanaditya@gmail.com>
2021-06-05 16:17:56 -0600

    added first working example

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,25 @@ mod fft;
 use num::complex::Complex;
 
 fn main() {
-    let a: Vec<u8> = vec![1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-    let b: Vec<u8> = vec![1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    let a: Vec<u8> = vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    let b: Vec<u8> = vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-    let z: fft::RootOfUnity = fft::RootOfUnity::new(a.len() * 2); 
+    let mut z: fft::RootOfUnity = fft::RootOfUnity::new((a.len() * 2) as isize); 
 
     let A = fft::fft(&a, &z);
     let B = fft::fft(&b, &z);
 
     let C: Vec<Complex<f64>> = A.iter().zip(B.iter()).map(|(&x, &y)| x * y).collect();
+    z.p = -1; 
 
-    println!("{:?}", C)
+    let D: Vec<Complex<f64>> = fft::ifft(&C, &z);
+    let mut E: Vec<f64> = Vec::new();
+
+    for i in D {
+        let e: Complex<f64> = i / Complex::new((a.len() * 2) as f64, 0.0);
+        E.push(e.re.round());
+    }
+
+    println!("{:?}", E)
+
 }
