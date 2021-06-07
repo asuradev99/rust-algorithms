de5309c623699818514d14658ef2f7f799f89c9c
Aditya Suresh <ethanaditya@gmail.com>
2021-06-07 17:04:22 -0600

    updates

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # Rust Algorithms
 This is a collection of algorithms I have implemented in Rust. 
 ## Current Project: Fast Fourier Transform for Polynomial Multiplication
-Todo: 
-- Use BigNum or another crate such as Ramp to handle bigger numbers more idiomatically
-- Refactor FFT code to remove duplicate ifft subroutine
-- Refactor the FFT poly driver code
-- Refactor the standard multiplication algorithm code
-- Compare the running times of the FFT poly driver and the standard algo (Criterion, gnuplot, bench)
-
 
 ## List of Algorithms Completed:
 ### Sorting
@@ -15,8 +8,11 @@ Todo:
 - Insertion Sort
 - Merge Sort
 
-## List of WIP Algorithms: 
+### Other 
 - Fast Fourier Transform
 - Standard Multiplication Algorithm (Long Multiplication)
+
+## List of WIP Algorithms: 
+
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -53,25 +53,7 @@ fn fft_test(b: &mut Bencher) {
         let a: Vec<u8> = (0..n).map(|_| rng.gen_range(0..10)).collect();
         let b: Vec<u8> = (0..n).map(|_| rng.gen_range(0..10)).collect();
 
-        println!("{:?} * {:?}", a, b);
-
-        let mut z: fft::RootOfUnity = fft::RootOfUnity::new((a.len() * 2) as isize);
-
-        let A = fft::fft(&a, &z);
-        let B = fft::fft(&b, &z);
-
-        let C: Vec<Complex<f64>> = A.iter().zip(B.iter()).map(|(&x, &y)| x * y).collect();
-        z.p = -1;
-
-        let D: Vec<Complex<f64>> = fft::ifft(&C, &z);
-        let mut E: Vec<f64> = Vec::new();
-
-        for i in D {
-            let e: Complex<f64> = i / Complex::new((a.len() * 2) as f64, 0.0);
-            E.push(e.re.round());
-        }
-
-        println!("{:?}, {:?}", E, E.len())
+        
     })
 }
 #[bench]
