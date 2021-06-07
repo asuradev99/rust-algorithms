5231fa4984581eadad2376674133a5467cdbf5d2
Aditya Suresh <ethanaditya@gmail.com>
2021-06-07 15:56:22 -0600

    minor refactoring

--- src/fft.rs
+++ src/fft.rs
@@ -1,7 +1,4 @@
-#![allow(dead_code, non_snake_case, unused_variables)]
-
 use std::f64::consts::PI;
-
 use num::complex::Complex;
 
 type C64 = Complex<f64>;
@@ -18,10 +15,9 @@ impl RootOfUnity {
         RootOfUnity { n: n, p: 1, k: 1 }
     }
     fn eval(&self) -> C64 {
-        // PRECOMPUTE e^(2pi) in future refactoring
         Complex::exp(Complex::new(
             0.0,
-            self.p as f64 * 2 as f64 * self.k as f64 * PI / self.n as f64,
+            (self.p * 2 * self.k) as f64 * PI / self.n as f64,
         ))
     }
     fn pow(&self, j: isize) -> RootOfUnity {
@@ -43,26 +39,22 @@ pub fn fft(p: &Vec<C64>, z: &RootOfUnity) -> Vec<C64> {
     }
     let E = fft(&a[0], &z.pow(2));
     let O = fft(&a[1], &z.pow(2));
-    let C = (z.n / z.k) as usize;
-    let mut P: Vec<C64> = vec![Complex::new(0.0, 0.0); C];
-    for i in 0..(C / 2) {
-        P[i as usize] = E[i as usize] + z.pow(i as isize).eval() * O[i as usize];
-        P[(i as usize + (C / 2))] = E[i] - z.pow(i as isize).eval() * O[i];
+    let c = (z.n / z.k) as usize;
+    let mut P: Vec<C64> = vec![Complex::new(0.0, 0.0); c];
+    for i in 0..(c / 2) {
+        P[i] = E[i] + z.pow(i as isize).eval() * O[i];
+        P[i + c / 2] = E[i] - z.pow(i as isize).eval() * O[i];
     }
     return P;
 }
 
 pub fn fft_poly(a: Vec<C64>, b: Vec<C64>) -> Vec<usize> {
-    let mut z: RootOfUnity = RootOfUnity::new((a.len() * 2) as isize);
+    let z: RootOfUnity = RootOfUnity::new((a.len() * 2) as isize);
     let A = fft(&a, &z);
     let B = fft(&b, &z);
-    let C: Vec<Complex<f64>> = A.iter().zip(B.iter()).map(|(&x, &y)| x * y).collect();
-    z.p = -1;
-    let D: Vec<Complex<f64>> = fft(&C, &z);
-    let mut E: Vec<usize> = Vec::new();
-    for i in D {
-        let e: Complex<f64> = i / Complex::new((a.len() * 2) as f64, 0.0);
-        E.push(e.re.round() as usize);
-    }
+    let C: Vec<C64> = A.iter().zip(B.iter()).map(|(&x, &y)| x * y).collect();
+    let C: Vec<C64> = fft(&C, &RootOfUnity{n: z.n, p: -1, k: z.k});
+    let E: Vec<usize> = C.iter().map(|&c|(c / Complex::new((a.len() * 2) as f64, 0.0)).re.round() as usize).collect();
     E
 }
+
